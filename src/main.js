0ffe3bd8feebcb01fd3e034835c6844da29168fb
Alisher Kasimov <alisherkasimov@gmail.com>
2020-10-09 12:16:47 +0500

    Added pagination with jw-vue-pagination;

--- src/main.js
+++ src/main.js
@@ -5,9 +5,12 @@ import router from './_router/routes'
 
 import Admin from './layouts/Admin-layout';
 import Customer from './layouts/Customer-layout';
+// register jw pagination component globally
+import JwPagination from 'jw-vue-pagination';
 
 Vue.component('admin-layout', Admin);
 Vue.component('customer-layout', Customer);
+Vue.component('jw-pagination', JwPagination);
 
 Vue.config.productionTip = false
 
